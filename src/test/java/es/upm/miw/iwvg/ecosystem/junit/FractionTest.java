836c7649ecdb3eda7769d8586690d46dbc41199e
codinoz <jnyupanqui@hotmail.com>
2019-06-15 05:10:57 +0000

    test completed

--- src/test/java/es/upm/miw/iwvg/ecosystem/junit/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg/ecosystem/junit/FractionTest.java
@@ -11,17 +11,32 @@ public class FractionTest {
     private Fraction fraction;
 
     @Test
+    void testFraction(){
+        fraction = new Fraction();
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(1, fraction.getDenominator());
+    }
+
+    @Test
     void testDecimalIfIntegerDivision(){
         fraction = new Fraction(5,2);
         assertEquals(2.5,fraction.decimal());
     }
 
+
     @Test
     void testDecimalIfDivisionByZero(){
         fraction = new Fraction(5,0);
         assertTrue(Double.isInfinite(fraction.decimal()));
+
         fraction = new Fraction(0,0);
         assertTrue(Double.isNaN(fraction.decimal()));
+    }
+
+    @Test
+    void testDecimalIfDivisionNegativeByZero() {
+        fraction = new Fraction(-5,0);
+        assertTrue(Double.isInfinite(fraction.decimal()));
     }
 
     @Test
@@ -29,13 +44,19 @@ public class FractionTest {
         fraction = new Fraction(0,2);
         assertEquals(0, fraction.decimal());
 
-        fraction = new Fraction(2,8);
-        assertEquals(0.25, fraction.decimal());
-
         fraction = new Fraction(8, 2);
         assertEquals(4, fraction.decimal());
 
         fraction = new Fraction();
         assertEquals(1, fraction.decimal());
+    }
+
+    @Test
+    void testDecimalNegative(){
+        fraction = new Fraction(0,-2);
+        assertEquals(-0.0, fraction.decimal());
+
+        fraction = new Fraction(-8, -2);
+        assertEquals(4, fraction.decimal());
     }
 }
